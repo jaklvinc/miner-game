a493ee324d746761144fec4216cc07683051adaf
Vincent <jaklvinc@fit.cvut.cz>
2021-03-03 18:52:29 +0100

    Works for basic data

---
+++ practice/pr3/sample/test.cpp
@@ -0,0 +1,75 @@
+#ifndef __PROGTEST__
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <stdexcept>
+#include <vector>
+#include <array>
+#include <cassert>
+using namespace std;
+#endif /* __PROGTEST__ */
+
+struct Contact
+{
+    string m_Name;
+    string m_Surname;
+    string m_Number;
+};
+
+bool report ( const string & fileName, ostream & out )
+{
+    ifstream file;
+    file.open(fileName);
+    if (!file)
+    {
+        cerr << "ERROR, can't read file!" << endl;
+        return false;
+    }
+    string dataline;
+    getline(file,dataline);
+
+
+
+    vector <Contact> Phonebook;
+    string Name;
+    int Times_found=0;
+
+    for (auto & Contact : Phonebook)
+    {
+        if (Contact.m_Name == Name || Contact.m_Surname == Name )
+        {
+            Times_found++;
+            out << Contact.m_Name << " " << Contact.m_Surname << " " << Contact.m_Number << "\n";
+        }
+    }
+    out << "-> " << Times_found << "\n";
+
+
+    file.close();
+  return true;
+}
+
+#ifndef __PROGTEST__
+int main ()
+{
+  ostringstream oss;
+  oss . str ( "" );
+  assert ( report( "tests/test0_in.txt", oss ) == true );
+  assert ( oss . str () ==
+    "John Christescu 258452362\n"
+    "John Harmson 861647702\n"
+    "-> 2\n"
+    "-> 0\n"
+    "Josh Dakhov 264112084\n"
+    "Dakhov Speechley 865216101\n"
+    "-> 2\n"
+    "John Harmson 861647702\n"
+    "-> 1\n" );
+  oss . str ( "" );
+  assert ( report( "tests/test1_in.txt", oss ) == false );
+  return 0;
+}
+#endif /* __PROGTEST__ */
