ff7feb0881b783bfb99a5ee7b56d30e1a4c25e22
Vincent <jaklvinc@fit.cvut.cz>
2021-03-28 23:21:39 +0200

    Works for one time limit

--- homework/hw03/test.cpp
+++ homework/hw03/test.cpp
@@ -25,6 +25,71 @@ bool cmp_double( double first , double second )
   return ( abs(first-second) < 0.1 );
 }
 
+pair<double,double> complex_multiply ( pair<double,double> first , pair<double,double> second )
+{
+  return make_pair( (first.first * second.first)-(first.second * second.second) , (first.first * second.second) + (first.second * second.first) );
+}
+
+pair<double,double> complex_plus ( pair<double,double> first , pair<double,double> second )
+{
+  return make_pair( (first.first + second.first) , (first.second + second.second) );
+}
+
+pair<double,double> complex_minus ( pair<double,double> first , pair<double,double> second )
+{
+  return make_pair( (first.first - second.first) , (first.second - second.second) );
+}
+
+
+// inspired by https://www.geeksforgeeks.org/fast-fourier-transformation-poynomial-multiplication/
+vector<pair<double,double>> FFT ( const vector<pair<double,double>> & transform_me , bool reverse )
+{
+  int size = transform_me.size();
+  if (size == 1)
+    return transform_me;
+  
+  // omega = e^((2*pi*i)/n)
+  vector<pair<double,double>> omega(size);
+  if ( !reverse )
+  {
+    for ( int i = 0 ; i < size ; i++ )
+    {
+      double alpha = -2 * M_PI * i / size;
+      omega[i].first=cos(alpha); 
+      omega[i].second=sin(alpha);
+    } 
+  }
+  else
+  {
+    for ( int i = 0 ; i < size ; i++ )
+    {
+      double alpha = -2 * M_PI * i / size;
+      omega[i].first=cos(alpha); 
+      omega[i].second=-sin(alpha);
+    } 
+  }
+  
+
+  vector<pair<double,double>> Pe(size / 2) , Po(size / 2);
+  for (int i = 0; i < size / 2; i++) 
+  {
+    Pe[i] = transform_me[i * 2];
+    Po[i] = transform_me[i * 2 + 1];
+  }
+
+  vector<pair<double,double>> Ye = FFT ( Pe , reverse );
+  vector<pair<double,double>> Yo = FFT ( Po , reverse );
+
+  vector<pair<double,double>> Y(size);
+
+  for (int k = 0; k < size / 2; k++) {
+        Y[k] = complex_plus ( Ye[k] , complex_multiply( omega[k] , Yo[k]) ) ;
+        Y[k + size / 2] = complex_minus ( Ye[k] , complex_multiply (omega[k] , Yo[k]) );
+    }
+  return Y;
+}
+
+
 class CPolynomial
 {
   public:
@@ -33,6 +98,47 @@ class CPolynomial
     {
       m_Indeces.push_back(0);
     }
+    // polynomial multiplication
+    CPolynomial multiply( const CPolynomial & first , const CPolynomial & second ) const
+    {
+      vector<pair<double,double>> complex_first;
+      vector<pair<double,double>> complex_second;
+
+      for ( auto i : first.m_Indeces )
+        complex_first.push_back(make_pair(i,0));
+      
+      for ( auto i : second.m_Indeces )
+        complex_second.push_back(make_pair(i,0));
+
+      size_t new_degree = first.Degree() + second.Degree() + 1 ;
+      while ( (new_degree & (new_degree-1)) != 0)
+        new_degree++;
+
+      while ( complex_first.size() < new_degree )
+        complex_first.push_back(make_pair(0,0));
+      
+      while ( complex_second.size() < new_degree )
+        complex_second.push_back(make_pair(0,0));
+      
+      vector<pair<double,double>> values_first = FFT ( complex_first , false ) ;
+      vector<pair<double,double>> values_second = FFT ( complex_second , false ) ;
+      vector<pair<double,double>> values_combined(values_first.size());
+      for ( size_t i = 0 ; i < values_combined.size() ; i++ )
+        values_combined[i] = complex_multiply ( values_first[i] , values_second[i] );
+      
+      vector<pair<double,double>> result = FFT ( values_combined , true ) ;
+     //for ( size_t i = 0 ; i < result.size() ; i++ )
+      //  result[i].first /= result.size();
+
+      CPolynomial ret;
+      ret.m_Indeces.resize(result.size());
+      for ( size_t i = 0 ; i < result.size() ; i++ )
+        ret.m_Indeces[i] = result[i].first / result.size();
+        //ret.m_Indeces[i] = abs(result[i].first / result.size()) > 0.1 ? result[i].first / result.size() : 0 ;
+      
+      return ret;
+    }
+    
     // cmp function
     bool cmp_Polyn (const CPolynomial & first , const CPolynomial & second ) const
     {
@@ -106,20 +212,7 @@ class CPolynomial
     // operator * (polynomial, double)
     CPolynomial operator * (const CPolynomial & polyn) const
     {
-      CPolynomial NewIndeces;
-      NewIndeces.m_Indeces.resize(this->m_Indeces.size()+polyn.m_Indeces.size());
-      for ( size_t i = 0 ; i < NewIndeces.m_Indeces.size() ; i++ )
-        NewIndeces.m_Indeces[i] = 0;
-      
-      for ( size_t i = 0 ; i < this->m_Indeces.size() ; i++ )
-      {
-        for ( size_t j = 0 ; j < polyn.m_Indeces.size() ; j++ )
-        {
-          NewIndeces.m_Indeces[i+j] += this->m_Indeces[i] * polyn.m_Indeces[j];
-        }
-      }
-      
-      return NewIndeces;
+      return multiply(*this,polyn);
     }
 
     CPolynomial operator * (const double multip) const
@@ -176,7 +269,7 @@ class CPolynomial
       size_t degree = 0;
       for ( size_t i = 0 ; i < this->m_Indeces.size() ; i++ )
       {
-        if ( abs(this->m_Indeces[i]) != 0 )
+        if ( abs(this->m_Indeces[i]) > 0.1 )
           degree = i;
       }
       return degree;
@@ -255,6 +348,7 @@ class CPolynomial
   
   private:
     vector<double> m_Indeces;
+    vector<double> m_Complex_Indeces;
 
     
 };
@@ -277,9 +371,13 @@ bool               dumpMatch                               ( const CPolynomial &
 
 int                main                                    ( void )
 {
-  CPolynomial a, b, c;
+  CPolynomial a, b, c , x , y;
   ostringstream out;
 
+  y[0] = 2;
+  y[1] = 4;
+  y[2] = 9;
+  x = x * y;
   a[0] = -10;
   a[1] = 3.5;
   a[3] = 1;
